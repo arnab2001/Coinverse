04f935f8f54198fdc6c689faeeb7940874194c92
eddybank <ehorbinski@gmail.com>
2023-02-04 04:09:29 -0500

    [#13] navbar-wrong-selection
    Made the code more readable by fixing indentation.
    
    Changed keys of items to their relative hrefs to set the default selected key based of current url.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,55 +30,54 @@ import './nav.css'
 
 
 const Navbar = () => {
-    const { Header, Sider, Content } = Layout;
 
-const [collapsed, setCollapsed] = useState(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
-const {
-  token: { colorBgContainer },
-} = theme.useToken();
+  const { Header, Sider, Content } = Layout;
+
+  const [collapsed, setCollapsed] = useState(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent));
+  const { token: { colorBgContainer } } = theme.useToken();
+
   return (
     <>
-    
-    
-
     <Layout className='main_component' >
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        
        <div className="logo" > 
-           <Row style={{width:"100%"}} type="flex" align="center">
-  <Col> <Image src={icon} preview={false} /></Col>
-           
+          <Row style={{width:"100%"}} type="flex" align="center">
+            <Col> 
+              <Image src={icon} preview={false} />
+            </Col>
             <Col>
-            <Typography.Title level={3} >
-                <Link to="/">Coinverse</Link>
-            </Typography.Title></Col></Row>
+              <Typography.Title level={3} >
+                  <Link to="/">Coinverse</Link>
+              </Typography.Title>
+            </Col>
+          </Row>
         </div>
         <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
+          theme="dark" 
+          mode="inline" 
+          defaultSelectedKeys={['/' + window.location.href.split('/')[3]]}
           items={[
             {
-              key: '1',
+              key: '/',
               icon: <HomeOutlined />,
-              label: 'home',
+              label: 'Home',
               onClick: () => window.location.href='/',
             },
             {
-              key: '2',
+              key: '/Cryptocurrencies',
               icon: <SlidersOutlined />,
                 label: 'Cryptocurrencies',
                 onClick: () => window.location.href='/Cryptocurrencies',
 
             },
             {
-              key: '3',
+              key: '/News',
               icon: <BulbOutlined/>,
               label: 'Crypto News',
               onClick: () => window.location.href='/News',
             },
           ]}
-          />
+        /> 
       </Sider>
       <Layout className="site-layout">
         <Header style={{ paddingLeft: "1rem", background: colorBgContainer ,fontSize:"1.8rem"}}>
@@ -88,53 +87,41 @@ const {
           })}
         </Header>
         <Content>
-     <div className="main" >
+          <div className="main" >
             <Layout>
-                <div className='routes'>
-                    <Routes>
-                        <Route path='/' element={<Homepage/>}/>
-                        <Route path='/Exchanges' element={<Exchanges/>}/>
-                        <Route path='/Cryptocurrencies' element={<Cryptocurrencies/>}/>
-                        <Route path='/crypto/:coinId' element={<CryptoDetailes/>}/>
-                        <Route path='/news' element={<News/>}/>
-                    </Routes>
-                </div>
-                
+              <div className='routes'>
+                <Routes>
+                  <Route path='/' element={<Homepage/>}/>
+                    <Route path='/Exchanges' element={<Exchanges/>}/>
+                    <Route path='/Cryptocurrencies' element={<Cryptocurrencies/>}/>
+                    <Route path='/crypto/:coinId' element={<CryptoDetailes/>}/>
+                    <Route path='/news' element={<News/>}/>
+                </Routes>
+              </div> 
             </Layout>
-
-            
             <Layout.Footer style={{ background: '#002547', color: 'white', padding: '20px' }}>
-      <Row>
-       <Col xs={10} sm={8} md={8} lg={6}>
-          <h3>Made with ❤️ by Arnab</h3>
-          <p>Using <a href="https://rapidapi.com/Coinranking/api/coinranking1" style={{ color: 'grey' }}> Coinranking API</a>
-          , <a href="https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-news-search1/" style={{ color: 'grey' }}>Bing News API </a> 
-          ,React,Ant Design,Redux and React Router.
-
-
-
-          </p>
-        </Col> 
-       
-        <Col>
-          <h3>Links</h3>
-          <a href="/" style={{ color: 'grey' }}>Home</a>
-          &nbsp; &nbsp;
-          <a href="/" style={{ color: 'grey' }}>Cryptocurrencies</a>
-          &nbsp; &nbsp;
-          <a href="/" style={{ color: 'grey' }}>Crypto News</a>
-        </Col>
-      </Row>
-    </Layout.Footer>
-        </div>
+              <Row>
+                <Col xs={10} sm={8} md={8} lg={6}>
+                  <h3>Made with ❤️ by Arnab</h3>
+                  <p>Using <a href="https://rapidapi.com/Coinranking/api/coinranking1" style={{ color: 'grey' }}> Coinranking API</a>
+                  , <a href="https://rapidapi.com/microsoft-azure-org-microsoft-cognitive-services/api/bing-news-search1/" style={{ color: 'grey' }}>Bing News API </a> 
+                  , React, Ant Design, Redux and React Router.</p>
+                </Col> 
+                <Col>
+                  <h3>Links</h3>
+                  <a href="/" style={{ color: 'grey' }}>Home</a>
+                  &nbsp; &nbsp;
+                  <a href="/Cryptocurrencies" style={{ color: 'grey' }}>Cryptocurrencies</a>
+                  &nbsp; &nbsp;
+                  <a href="/News" style={{ color: 'grey' }}>Crypto News</a>
+                </Col>
+              </Row>
+            </Layout.Footer>
+          </div>
         </Content> 
       </Layout>
     </Layout>
-  
-
-  </>
-
-    
+  </> 
 )
 }
 
