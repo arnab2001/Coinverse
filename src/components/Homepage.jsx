62f7f3f98c7ca405dcb9fd25614a5a426b0eb779
Rajdeep Chowdhury <rajdeepchowdhury318@gmail.com>
2023-03-07 12:41:30 +0530

    global crypto stats responsive cards

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -25,11 +25,76 @@ const Homepage = ({onClick}) => {
         Global Crypto Stats
       </Title>
       <Row gutter={[32,7]} >
-        <Col span={8}><Statistic title="Total Cryptocurrencies" value={globalStats.total} /></Col>
-        <Col span={8}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)} /></Col>
-        <Col span={8}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)} /></Col>
-        <Col span={8}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)} /></Col>
-        <Col span={8}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+        <Col span={8}>
+          <div class="ag-format-container">
+            <div class="ag-courses_box">
+              <div class="ag-courses_item">
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+                  <div class="ag-courses-item_title">
+                  <Statistic title="Total Crypto Currencies" value={globalStats.total}/>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={8}>
+          <div class="ag-format-container">
+            <div class="ag-courses_box">
+              <div class="ag-courses_item">
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+                  <div class="ag-courses-item_title">
+                  <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={8}>
+          <div class="ag-format-container">
+            <div class="ag-courses_box">
+              <div class="ag-courses_item">
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+                  <div class="ag-courses-item_title">
+                  <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={8}>
+          <div class="ag-format-container">
+            <div class="ag-courses_box">
+              <div class="ag-courses_item">
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+                  <div class="ag-courses-item_title">
+                  <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col span={8}>
+          <div class="ag-format-container">
+            <div class="ag-courses_box">
+              <div class="ag-courses_item">
+                <a href="#" class="ag-courses-item_link">
+                  <div class="ag-courses-item_bg"></div>
+                  <div class="ag-courses-item_title">
+                  <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </Col>
       </Row>
       </div>
       <div className="home-heading-container">
