5c702f7bd0e4af8aef583c573bc9febfccb62df0
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2023-03-11 22:02:11 +0530

    Added more cypress testing cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -24,4 +24,36 @@ describe('Coinverse Testing', () => {
     cy.url().should('include', '/Cryptocurrencies');
   });
 
+  it('Clicking on `Home` button', () => {
+    cy.visit("0.0.0.0:3000");
+    cy.contains('Home').click();
+    cy.url().should('include', '/');
+  })
+
+  it('Clicking on `News` button', () => {
+    cy.visit("0.0.0.0:3000");
+    cy.contains('News').click();
+    cy.url().should('include', '/News');
+  })
+
+  it('Clicking on `Show more` button', () => {
+    cy.visit("0.0.0.0:3000");
+    cy.get('#show-more-text').click();
+    cy.url().should('include', '/Cryptocurrencies');
+  })
+
+  it('Testing the search button', () => {
+    cy.visit("0.0.0.0:3000/Cryptocurrencies");
+    cy.get(".search-crypto input").type("search any cryptocurrency");
+  })
+
+  it('Stats heading testing', () => {
+    cy.visit("0.0.0.0:3000");
+    cy.get(".top-heading h2").contains('Global Crypto Stats');
+  })
+
+  it("Testing bookmark button", () => {
+    cy.visit("0.0.0.0:3000");
+    cy.get('span.anticon.anticon-book').click({multiple: true});
+  })
 })
